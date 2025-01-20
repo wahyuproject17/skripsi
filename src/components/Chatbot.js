2d19182866d45e4293af446033d01db9c9d6a7de
wahyuproject17 <wahyuaguz9@gmail.com>
2025-01-20 13:52:15 +0700

    update chatbot

--- src/components/Chatbot.js
+++ src/components/Chatbot.js
@@ -147,7 +147,7 @@ const Chatbot = () => {
   
       try {
         // Kirim pesan ke backend API
-        const response = await axios.post('https://94de-34-125-114-23.ngrok-free.app/api/chatbot', {
+        const response = await axios.post('https://2145-35-186-189-191.ngrok-free.app/api/chatbot', {
           question: messageText,
         });
   
